7e0b5d176eaa73f98b6bd1f79365fe3c9d2ffcc4
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-09-27 11:47:39 +0300

    ДЗ 12. Поиск символа в строке

--- HW_12.py
+++ HW_12.py
@@ -1,15 +1,17 @@
 str = input('Введите пожалуйста строку: ')
 symb = input('Введите пожалуйста искомый символ: ')
-index = -1
+search = -1
 quant = 0
-for new_str in str:
-    index += 1
-    search = new_str.find(symb)
-    if search != -1:
-        quant += 1
-        if quant == 1:
-            print('Индекс указанного символа: ', end=" ")
-    if search != -1:
-        print(index, end=" ")
+while True:
+    search = str.find(symb, search + 1)
+    if search == -1:
+        break
+    quant += 1
+    if quant == 1:
+        print('\nИндекс указанного символа: ', end=" ")
+    print(search, end=" ")
+if quant == 0:
+    print('\nИскомый символ в строке не найден')
+print()
 if quant > 0:
     print("\nКоличество раз вхождений указанного символа: ", quant)
